71a24f819c21c48362e8194401e2e8332a4813cd
eightywon <scott.neitzel@gmail.com>
2017-12-12 09:03:38 -0600

    0.0.1

--- node_helper.js
+++ node_helper.js
@@ -1,9 +1,13 @@
 'use strict';
 
 /* Magic Mirror
- * Module: MMM-PIR-Sensor
+ * Module: MMM-PIR-Sensor-BlankIt
+ * 
+ * By Scott Neitzel http://eightywon.org
  *
+ * Forked from:
  * By Paul-Vincent Roll http://paulvincentroll.com
+ *
  * MIT Licensed.
  */
 
@@ -16,75 +20,31 @@ module.exports = NodeHelper.create({
     this.started = false;
   },
 
-  activateMonitor: function () {
-    if (this.config.relayPIN != false) {
-      this.relay.writeSync(this.config.relayOnState);
-    }
-    else if (this.config.relayPIN == false){
-      // Check if hdmi output is already on
-      exec("/opt/vc/bin/tvservice -s").stdout.on('data', function(data) {
-        if (data.indexOf("0x120002") !== -1)
-          exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
-      });
-    }
-  },
-
-  deactivateMonitor: function () {
-    if (this.config.relayPIN != false) {
-      this.relay.writeSync(this.config.relayOffState);
-    }
-    else if (this.config.relayPIN == false){
-      exec("/opt/vc/bin/tvservice -o", null);
-    }
-  },
-
   // Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
     if (notification === 'CONFIG' && this.started == false) {
-      const self = this;
-      this.config = payload;
+     const self = this;
+     this.config = payload;
 
-      // Setup value which represent on and off
-      const valueOn = this.config.invertSensorValue ? 0 : 1;
-      const valueOff = this.config.invertSensorValue ? 1 : 0;
+     // Setup value which represent on and off
+     const valueOn = this.config.invertSensorValue ? 0 : 1;
+     const valueOff = this.config.invertSensorValue ? 1 : 0;
 
-      //Setup pins
-      this.pir = new Gpio(this.config.sensorPIN, 'in', 'both');
-      // exec("echo '" + this.config.sensorPIN.toString() + "' > /sys/class/gpio/export", null);
-      // exec("echo 'in' > /sys/class/gpio/gpio" + this.config.sensorPIN.toString() + "/direction", null);
+     //Setup pins
+     this.pir = new Gpio(this.config.sensorPIN, 'in', 'both');
 
-      if (this.config.relayPIN) {
-        this.relay = new Gpio(this.config.relayPIN, 'out');
-        this.relay.writeSync(this.config.relayOnState);
-        exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
+     //Detected movement
+     this.pir.watch(function(err, value) {
+      if (value==valueOn) {
+       clearTimeout(self.deactivateMonitorTimeout);
+       self.sendSocketNotification("USER_PRESENCE",true);
+      } else if (value==valueOff) {
+       self.deactivateMonitorTimeout=setTimeout(function() {
+        self.sendSocketNotification("USER_PRESENCE",false);
+       }, self.config.powerSavingDelay*1000);
       }
-
-      //Detected movement
-      this.pir.watch(function(err, value) {
-        if (value == valueOn) {
-          self.sendSocketNotification("USER_PRESENCE", true);
-          if (self.config.powerSaving){
-            clearTimeout(self.deactivateMonitorTimeout);
-            self.activateMonitor();
-          }
-        }
-        else if (value == valueOff) {
-          self.sendSocketNotification("USER_PRESENCE", false);
-          if (!self.config.powerSaving){
-            return;
-          }
-
-          self.deactivateMonitorTimeout = setTimeout(function() {
-            self.deactivateMonitor();
-          }, self.config.powerSavingDelay * 1000);
-        }
-      });
-
-      this.started = true;
-
-    } else if (notification === 'SCREEN_WAKEUP') {
-      this.activateMonitor();
+     });
+     this.started=true;
     }
   }
-
 });
